64ce13be949d8f0f438c38b177f1cc0757f52733
Ouadi Wassim <ouadiwassim@MacBook-Air-de-Ouadi-2.local>
2023-05-11 15:21:37 +0200

    1er commit : fichiers Main.java et classe Personne

---
+++ Main.java
@@ -0,0 +1,56 @@
+public class Main {
+    public static void main(String[] args) {
+        /** Lorsqu'on déclare une variable de type objet, on dit
+         * que l'on fait une INSTANCIATION.
+         * Un objet est une INSTANCE d'une classe. */
+        Personne p = new Personne("Onyme", "Anne", 50);
+        // p.prenom = "Anne";
+        // p.nom = "Onyme";
+        // p.age = 50;
+        System.out.println("Nom : " + p.nom);
+        System.out.println("Prénom : " + p.prenom);
+        System.out.println("Age : " + p.getAge());
+        
+        System.out.println(p.identite());
+        Personne p1 = new Personne("Jean", "Cérien");
+        //p1.prenom = "Jean";
+        //p1.nom = "Cérien";
+        p1.setAge(25);
+        System.out.println("Identité de p1 : " + p1.identite());
+
+        /*
+         * EXERCICES :
+         * 1. Déclarer un tableau d'objets Personne.
+         * 2. Remplir le tableau avec les données utilisées 
+         *    dans exempleArray.java 
+         * 3. Afficher toutes les identités des personnes (en utilisant la méthode identite())
+         * 4. Ajouter un constructeur qui permet d'instancier
+         *      une personne en définissant directement son prénom,
+         *      son nom et son âge.  
+         */
+        Personne[] tableauPersonnes = new Personne[10];
+        for (int i = 0; i < tableauPersonnes.length; i++) {
+            //tableauPersonnes[i] = new Personne();   
+        }
+
+        tableauPersonnes[0].prenom = "Gérard";
+        tableauPersonnes[0].nom = "Menfin";
+        tableauPersonnes[0].setAge(35);
+        //tableauPersonnes[1] = new Personne();
+        tableauPersonnes[1].prenom = "Nordine";
+        tableauPersonnes[1].nom = "Ateur";
+        tableauPersonnes[1].setAge(20);
+
+        for (Personne personne : tableauPersonnes) {
+            System.out.println(personne.identite() + " " + personne.getAge());
+        }
+        System.out.println(tableauPersonnes[1].identite());
+        /* NULL est un type de donnée particulier (qu'on peut considérer 
+            comme une valeur particulière).
+            Une variable qui n'a pas été initialisée est considéré comme
+            étant NULL.
+         */
+    }
+
+
+}
---
+++ Personne.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+public class Personne {
+    /**
+     *  Les propriétés (ou attributs) définies dans une classe
+     * correspondent aux caractéristiques des objets qui vont
+     * être créés à partir de cette classe.
+     */
+    public String prenom = "défaut";
+    public String nom;
+    private int age;
+
+    /** CONSTRUCTEUR
+     * Un  constructeur est une méthode qui est exécutée quand un objet est instancié.
+     * Toutes les classes ont un constructeur par défaut dans Java. 
+     * CE CONSTRUCTEUR A LE MÊME NOM QUE LA CLASSE. Par exemple :
+     * Personne p = new Personne();
+     * 
+     * On peut définir un nouveau constructeur.
+     * ⚠️ Un constructeur ne DOIT PAS retourner une valeur. Donc il n'y  pas de type 
+         avant le nom de la méthode lors de la déclaration.
+     * En général, les constructeurs sont utilisés pour initialiser la valeur des 
+         propriétés de l'objet. 
+     */
+    public Personne(String name, String firstname, int old) {
+        nom = name;
+        prenom = firstname;
+        age = old;
+    }
+
+    /** 
+     * Quand on définit un constructeur, le constructeur par défaut n'est plus accessible.
+     * Si on veut l'utiliser à nouveau, il faut déclarer un constructeur sans argument.
+     * NB : on peut laisser les {} vides si aucune instruction ne doit être exécutée.
+     * PS : on peut déclarer autant de constructeurs que l'on veut 
+     */
+    public Personne() {}
+    
+    public Personne(String prenom, String nom) {
+        this.prenom = prenom;
+        this.nom = nom;
+    }
+
+    /**
+     * Une fonction est dans une classe est appelée une MÉTHODE.
+     *  @return String concaténation du prénom et du nom
+     */
+    public String identite() {
+         return this.prenom + " " + this.nom;
+    }
+
+    public String identite(boolean vrai) {
+        return this.prenom + " " + this.nom + ", agé de " + this.age + " ans";
+    }    
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
